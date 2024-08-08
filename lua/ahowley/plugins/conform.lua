7e8be6639efb6045094a168c8b21d79cda5f73cc
Alex Howley <ahowley@nobilisgroup.com>
2024-08-08 12:22:40 -0500

    prioritize ruff for python

--- lua/ahowley/plugins/conform.lua
+++ lua/ahowley/plugins/conform.lua
@@ -29,7 +29,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform can also run multiple formatters sequentially
-      python = { "black" },
+      python = { "ruff", "black" },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
