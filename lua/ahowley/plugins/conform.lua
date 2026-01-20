28fe11ec585b1c8348a2e64d3621c032cca55c48
Alex Howley <ahowley@nobilisgroup.com>
2026-01-20 15:48:04 -0600

    fix csharpier again

--- lua/ahowley/plugins/conform.lua
+++ lua/ahowley/plugins/conform.lua
@@ -64,5 +64,26 @@ return { -- Autoformat
       sql = { "sleek" },
       cs = { "csharpier" },
     },
+    formatters = {
+      csharpier = function()
+        local useDotnet = not vim.fn.executable("csharpier")
+
+        local command = useDotnet and "dotnet csharpier" or "csharpier"
+
+        local version_out = vim.fn.system(command .. " --version")
+
+        local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
+        local is_new = major_version >= 1
+
+        local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
+
+        return {
+          command = command,
+          args = args,
+          stdin = not is_new,
+          require_cwd = false,
+        }
+      end,
+    },
   },
 }
