770dc9071d816ec813872d68afb6802d544642de
Alex Howley <ahowley@nobilisgroup.com>
2026-07-09 09:49:01 -0500

    dadbod updates and theme refresh

--- lua/ahowley/plugins/vim-dadbod-ui.lua
+++ lua/ahowley/plugins/vim-dadbod-ui.lua
@@ -29,6 +29,22 @@ return {
         name = "qa",
         url = vim.env.DB_QA,
       },
+      {
+        name = "dev-staging",
+        url = vim.env.DB_DEV_STAGING,
+      },
+      {
+        name = "dev",
+        url = vim.env.DB_DEV,
+      },
+      {
+        name = "sdbx-staging",
+        url = vim.env.DB_SDBX_STAGING,
+      },
+      {
+        name = "sdbx",
+        url = vim.env.DB_SDBX,
+      },
     }
   end,
 }
